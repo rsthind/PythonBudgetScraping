cbb341815b17696c6efff07f03dd950e289f0cb5
Ranveer Thind <ranveerthind01@hotmail.com>
2020-02-16 15:19:57 -0500

    Updating Spreadsheet

--- product_price.py
+++ product_price.py
@@ -1,13 +1,35 @@
+from amazon_scrape import AmazonScrape
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+class PriceUpdater(object):
+    def __init__(self, spreadsheet_name):
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        self.item_col = 1
+        self.price_col = 2
+        self.frequency_col = 3
+        self.url_col = 4
+        self.product_name_col = 5
 
-client = gspread.authorize(creds)
+        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-sheet = client.open('ProductPrice').sheet1
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 
-stuff = sheet.get_all_records()
-print(stuff)
+        client = gspread.authorize(creds)
+
+        self.sheet = client.open(spreadsheet_name).sheet1
+
+    def process_item_list(self):
+        items = self.sheet.col_values(self.item_col)[1:]
+
+        amazon_scrape = AmazonScrape(items)
+        prices, urls, names = amazon_scrape.search_items()
+
+        print("Updating spreadsheet.")
+        for i in range(len(prices)):
+            self.sheet.update_cell(i+2, self.price_col, prices[i])
+            self.sheet.update_cell(i+2, self.url_col, urls[i])
+            self.sheet.update_cell(i+2, self.product_name_col, names[i])
+
+price_updater = PriceUpdater("ProductPrice")
+price_updater.process_item_list()
